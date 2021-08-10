e9e5ac9bccaabad20f843f0a7f8a7d54cb8a2f14
LucianoWerneck <lulu_werneck@gmail.com.br>
2021-08-09 23:05:50 -0300

    Função para fazer escrita de titulo.

---
+++ funçãoTitulo.py
@@ -0,0 +1,11 @@
+def titulo(msg):
+    tam = len(msg)+4
+    print('~' * tam)
+    print(f'  {msg}')
+    print('~' * tam)
+
+
+titulo('Luciano Werneck Bartole')
+titulo('Comida')
+titulo('Luna')
+titulo('Isis')
