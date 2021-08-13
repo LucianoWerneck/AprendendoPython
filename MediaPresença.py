7192f3cd8c9f9faf532d5afd677b5fbd833b0e4b
LucianoWerneck <lulu_werneck@gmail.com.br>
2021-08-13 20:34:04 -0300

    Media do aluno e presença.

--- MediaPresença.py
+++ MediaPresença.py
@@ -6,7 +6,7 @@ faltas = int(input('Quantas Faltas: '))
 media = (nota1 + nota2) / 2
 presença = ((faltas*100/30)*-1+100)
 if media >= 6 and presença >= 70:
-    print(f'O aluno {nome} foi aprovado com {media} media e {"%.2f" % presença}% de presença.')
+    print(f'O aluno {nome} foi aprovado com {media} media com {"%.0f" % presença}% de presença.')
 elif media <= 6 and presença < 70:
     print(f'Aluno {nome} foi reprovado por meida baixa e excesso de faltas.')
 elif media < 6:
