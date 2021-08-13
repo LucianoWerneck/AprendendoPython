e6cde71ad486311fa56b8a62e51b20051caf3e77
LucianoWerneck <lulu_werneck@gmail.com.br>
2021-08-13 19:50:13 -0300

    Tradutor de Cores Português para Inglês

--- tradutorCores.py
+++ tradutorCores.py
@@ -1,13 +1,14 @@
 resp = 'S'
-cores = {'preto': 'black', 'verde': 'green', 'vermelho': 'red', 'branco': 'white',
-         'rosa': 'pink', 'roxa': 'purple', 'marron': 'brown', 'cinza': 'gray',
-         'prata': 'silver', 'dourada': 'golden', 'amarelo': 'yellow', 'azul': 'blue'}
+cores = {'preto': 'black', 'verde': 'green', 'vermelho': 'red',
+         'branco': 'white', 'rosa': 'pink', 'roxa': 'purple',
+         'marron': 'brown', 'cinza': 'gray', 'prata': 'silver',
+         'dourada': 'golden', 'amarelo': 'yellow', 'azul': 'blue'}
 while True:
     cor = input('Escolha a cor que deseja traduzir:\n').lower()
     tradução = cores.get(cor, 'Esta cor não consta no meu dicionario')
     print(tradução)
-    print('-'* 25)
+    print('-' * 25)
     resp = str(input('Deseja continuar(S/N): ').upper())
-    print('-'*25)
+    print('-' * 25)
     if resp == 'N':
         break
