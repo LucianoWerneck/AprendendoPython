0fb2c40a56ece6e15421f50cb494a5210147d8b5
LucianoWerneck <lulu_werneck@gmail.com.br>
2021-08-13 22:33:42 -0300

    Media do aluno e presença com validação dos dados.

--- MediaPresença.py
+++ MediaPresença.py
@@ -1,10 +1,43 @@
-'''Media e presença do Aluno'''
+'''Media e presença do Aluno Com Validação'''
 nome = str(input('Nome do aluno: '))
-nota1 = float(input('Nota 1ª Prova: '))
-nota2 = float(input('Nota 2ª Prova: '))
-faltas = int(input('Quantas Faltas: '))
+
+valid_nota = False
+while valid_nota == False:
+    nota1 = input('Digite a Nota da 1ª Prova: ')
+    try:
+        nota1 = float(nota1)
+        if nota1 < 0 or nota1 > 10:
+            print('Nota inválida. Valor deve ser entre 0 e 10.')
+        else:
+            valid_nota = True
+    except:
+        print('Nota inválida. Use apenas números e separe os decimais com ponto. (Ex. 9.5).')
+valid_nota = False
+while valid_nota == False:
+    nota2 = input('Digite a Nota da 2ª Prova: ')
+    try:
+        nota2 = float(nota2)
+        if nota2 < 0 or nota2 > 10:
+            print('Nota inválida. Valor deve ser entre 0 e 10.')
+        else:
+            valid_nota = True
+    except:
+        print('Nota inválida. Use apenas números e separe os decimais com pornto. (Ex. 9.5).')
+
+valid_faltas = False
+while valid_faltas == False:
+    faltas = input('Quantas Faltas o aluno teve: ')
+    try:
+        faltas = float(faltas)
+        if faltas < 0 or faltas > 30:
+            print('Número de faltas inválido. Valor de ser entre 0 e 30.')
+        else:
+            valid_faltas = True
+    except:
+        print('Número de faltas inválido. Use apenas números e separe os decimais com ponto.')
+
 media = (nota1 + nota2) / 2
-presença = ((faltas*100/30)*-1+100)
+presença = ((30 - faltas)/30)*100
 if media >= 6 and presença >= 70:
     print(f'O aluno {nome} foi aprovado com media {media} e com {"%.0f" % presença}% de presença.')
 elif media <= 6 and presença < 70:
